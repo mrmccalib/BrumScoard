9e6bd183d4b79af2040bb9f218cd1b2b4e64c20a
mrmccalib <mrmccalib@csbsju.edu>
2017-02-27 23:52:29 -0600

    very basic test suite

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert false
-   end
+
+    test "user existence" do
+      puts "Test existence of user 298486374"
+        begin
+          user = assert User.find(298486374)
+          puts "user exists!"
+        rescue
+          puts "something went wrong - user with ID 298486374 exists in db but assertion failed"
+          assert false
+        end
+    end
+
+    test "user nonexistence" do
+      puts "testing nonexistence"
+        begin
+          user = User.find(123455667)
+        rescue
+          puts "User with ID 123455667 does not exist in database"
+          assert true
+        end
+    end
+
 end
