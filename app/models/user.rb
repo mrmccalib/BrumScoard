539b90f9b422104c9b00875256f8b698e9902904
Matthew Lynch <mslynch95@gmail.com>
2017-03-19 15:33:10 -0500

    added error messages for registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :password, presence: true
     validates :username, uniqueness: true
+    #
+    # def validate!
+    #     if username.nil?
+    #         errors.add(:name, :blank, message: "Username cannot be empty!")
+    #     end
+    #     if password.nil?
+    #         errors.add(:name, :blank, message: "Username cannot be empty!")
+    #     end
+    # end
 end
