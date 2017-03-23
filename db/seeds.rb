8a1ccf1ee3745b8769aee4b0ce2f36108667ee73
mrmccalib <mrmccalib@csbsju.edu>
2017-03-22 20:09:20 -0500

    added kenzie and haylie to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 User.create(username: 'matthew', password: 'pass', first: 'Matthew', last: 'Lynch')
 User.create(username: 'michael', password: 'matthewiscool', first: 'Michael', last: 'McCalib')
 User.create(username: 'imax', password: 'brumscoardrocks', first: 'Imax', last: 'Rahal')
+User.create(username: 'kenzie', password: 'greenhair', first: 'Kenzie', last: 'Flickinger')
+User.create(username: 'haylie', password: 'overwatchmemes', first: 'Haylie', last: 'Murphey')
