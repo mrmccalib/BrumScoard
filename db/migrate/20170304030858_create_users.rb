a43e88d70dfcd607fdb6f55e8dc177a97bc8f675
Matthew Lynch <mslynch95@gmail.com>
2017-03-25 18:31:10 -0500

    migration changes

--- db/migrate/20170304030858_create_users.rb
+++ db/migrate/20170304030858_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     #drop_table :users
-    create_table :users, id: false do |t|
+    create_table :users do |t|
       t.string :username, null: false
     #   t.string :password, null: false
     #   t.string :password_digest
@@ -10,6 +10,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     end
 
     add_column :users, :password_digest, :string
-    execute "ALTER TABLE users ADD PRIMARY KEY (username);"
+    # execute "ALTER TABLE users ADD PRIMARY KEY (username);"
   end
 end
---
+++ db/migrate/20170325224047_create_join_table_boards_users.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableBoardsUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_join_table :boards, :users do |t|
+    #   t.index [:username, :name]
+    #   t.index [:name, :username]
+    end
+  end
+end
