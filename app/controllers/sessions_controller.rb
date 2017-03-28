3e6ede87c1b4e5adf1a84a7eae791a8391f09ee1
mrmccalib <mrmccalib@csbsju.edu>
2017-03-27 20:28:08 -0500

    added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,5 +19,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+      log_out
+      redirect_to login_url
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,11 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def log_out
+        session.delet(:user_id)
+        @current_user = nil
+    end
+
     # Returns the current logged-in user (if any).
     def current_user
         @current_user = @current_user || User.find_by(id: session[:user_id])
