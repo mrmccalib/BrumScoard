d4e1d530cac90d495fcb5ab43738490df317ca05
mrmccalib <mrmccalib@csbsju.edu>
2017-05-01 13:37:50 -0500

    styling is better but still not great

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+    layout 'sessions'
+
     def create
         user = User.find_by(username: params[:session][:username].downcase)
         if params[:session][:username].empty?
